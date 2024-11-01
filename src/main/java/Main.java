22217ffc354fc3ff135262a6f3071fbabfe7e0e5
JS <Yla45@yandex.ru>
2024-11-01 12:36:06 +0300

    Seminar 2

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> stringClass = Class.forName("java.lang.String");
+
+        Method[] methods = stringClass.getDeclaredMethods();
+
+        for(Method method : methods) {
+            System.out.println(method.getName());
+        }
+    }
+}
